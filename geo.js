8a5b18f3a04a2bd52437405e92f8a73505e45bf6
Kartikay-12 <kartikay_2101ce32@iitp.ac.in>
2023-03-09 21:25:47 +0530

    first

---
+++ geo.js
@@ -0,0 +1,8 @@
+
+
+const q = async () => {
+    const currentUrl = require('./new')
+    const k = await currentUrl;
+    console.log(k)
+
+}
---
+++ test.js
@@ -0,0 +1,5 @@
+var time =0;
+  if (true){
+    time=2000;
+  }
+  console.log(time)
