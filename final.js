c0d42eaf4900e39bd528b7bca31233badd4b6aff
Kartikay-12 <kartikay_2101ce32@iitp.ac.in>
2023-03-10 05:55:17 +0530

    0

--- final.js
+++ final.js
@@ -9,9 +9,7 @@ app.use(express.urlencoded({extended:true}))
 const {join} = require('path');
 
 
-/**
- * @type {import("puppeteer").Configuration}
- */
+
 module.exports = {
   // Changes the cache location for Puppeteer.
   cacheDirectory: join(__dirname, '.cache', 'puppeteer'),
